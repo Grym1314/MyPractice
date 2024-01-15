79aeb2fac05905fdc9f689c0d6bcb513cbb65c4e
Grym1314 <chucho_031199@hotmail.com>
2024-01-14 19:52:43 -0600

    Eloquent practice 14/01/24_01

---
+++ language.js
@@ -0,0 +1,13 @@
+//first example of code in Eloquent Introduction Chapter,
+
+let total = 0, count = 1; 
+while (count <=10){
+    total += count;
+    count += 1;
+}
+
+console.log (total);
+//As the book told use or as you can see, coding itcoul be create it in different ways
+// in the next lines you can see the same process but shortly and easy to compose
+
+console.log(sum(range(1,10)));
