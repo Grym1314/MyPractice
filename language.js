8f0f4eb5aaf3bf7dba1b703c0f430c26c3d422ef
Grym1314 <chucho_031199@hotmail.com>
2024-01-15 15:10:33 -0600

    Eloquent practice 15/01/24_03

--- language.js
+++ language.js
@@ -12,6 +12,7 @@ console.log (total);
 
 console.log(sum(range(1,10)));
 
+//Chapter 1.-- Values, Types, and operators
 //'Value' values in Javascript are chunks that represent pieces of information
 
 //01/15/24-Eloquent
@@ -35,8 +36,43 @@ console.log (false || false ) //false
 // the || will return the value to its left when that can be coverted to true and will return the value on its right other wise
 //Examples
 console.log(null || "user"); // user
-console.log("Agnes" || 'User')
+console.log("Agnes" || 'user'); // Agnes
 
 //Conditional Operator- it is representing by a ?
 console.log (true ? 1:2);// 1
 console.log (false ? 1:2);// 2
+
+//for this first chapter we used 10 console.log in total
+
+//CHAPTER 2 Program structure
+
+//"Let" indicates that this senteces is going to define a binding. It is followed by the name of the binding and if we want to immediately
+//give it a value, by an = operator and an expresion.
+
+let ten = 10;
+console.log (ten * ten);
+//Equals 100
+
+//the value is not forever, example:
+let mood = 'light';
+console.log (mood);  //--> Light
+
+mood = "Dark";
+console.log (mood);  //--> Dark
+
+//more examples
+let leslieDebt = 100;
+leslieDebt = leslieDebt - 10;
+console.log(leslieDebt); // 90
+
+// ',' comma can separate different definitions. Example:
+let one=1, two=2;
+console.log(one + two); // 3
+
+// const and var
+var nametwo = 'Jesus';
+const greeting = 'Welcome to my page ';
+console.log (greeting + nametwo);
+
+
+
