cc8c7b0c83e224f7903dc28f9e1c9690f05f0fd2
Grym1314 <chucho_031199@hotmail.com>
2024-01-15 13:50:22 -0600

    Eloquent practice 15/01/24_02

--- language.js
+++ language.js
@@ -21,3 +21,22 @@ console.log(sum(range(1,10)));
 console.log(typeof 4.5) //Number
 console.log(typeof "X") //String
 
+//Logical operators- supports three logical operator: and, or and not
+
+//the && operator represent logican and.
+//it is a binary operator and its result is true only if both the values given to it are TRUE
+console.log (true && false) //false
+console.log (true && true) //true
+
+//The operator || detonates logical or. 
+//It produces true if either of the values given to it is TRUE
+console.log(false || true ) //true
+console.log (false || false ) //false
+// the || will return the value to its left when that can be coverted to true and will return the value on its right other wise
+//Examples
+console.log(null || "user"); // user
+console.log("Agnes" || 'User')
+
+//Conditional Operator- it is representing by a ?
+console.log (true ? 1:2);// 1
+console.log (false ? 1:2);// 2
