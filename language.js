a37ea458853a3415469dde62c133235c38b7decc
Grym1314 <chucho_031199@hotmail.com>
2024-01-16 20:36:41 -0600

    Eloquent practice 16/01/24_05

--- language.js
+++ language.js
@@ -164,4 +164,19 @@ while (counterE <= 6) {
   console.log(numberLoopE);
   counterE++;
   numberLoopE = numberLoopE + "0";
+};
+
+let size = 9;
+let board = "";
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
 }
+
+console.log(board);
