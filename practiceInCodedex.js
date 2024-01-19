dd8890f136332053a83de228d6ca1147266ea4e5
Grym1314 <chucho_031199@hotmail.com>
2024-01-18 21:24:55 -0600

    Codedex practice 18/01/24_01

---
+++ practiceInCodedex.js
@@ -0,0 +1,13 @@
+// This is the URL if you want to learn by games https://www.codedex.io/javascript
+
+//Codedex
+//Let & Const
+
+//First practice
+const greetings = 'Hello good afternoon';
+const ask = 'What is you name?';
+let name = "Jesus";
+let lastName = 'Ranmirez';
+const askSecond = 'Do you work?';
+
+console.log(greetings, ask,name,lastName,askSecond);
