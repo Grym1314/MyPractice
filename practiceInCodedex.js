f4334efaeb98278e95cb5675d8fa6e35317d3d78
Grym1314 <chucho_031199@hotmail.com>
2024-01-19 20:13:11 -0600

    Codedex practice 19/01/24_01

--- practiceInCodedex.js
+++ practiceInCodedex.js
@@ -11,3 +11,20 @@ let lastName = 'Ranmirez';
 const askSecond = 'Do you work?';
 
 console.log(greetings, ask,name,lastName,askSecond);
+
+// 01/19/24
+//Growing Fuction
+function printFarmInventory (cows, chikens){
+    let cowString = String (cows);
+    while (cowString.length < 3){
+        cowString = '0' + cowString;
+    }
+    console.log (`${cowString} Cows`);
+
+    let chickenString = String(chikens);
+    while (chickenString.length < 3){
+        chickenString = '0' + chickenString;
+    }
+    console.log (`${chickenString} Chikens`);
+} 
+printFarmInventory (7,11);
