b112f4f6e5146621fa948ea8546d5a4fdf90be9b
Grym1314 <chucho_031199@hotmail.com>
2024-02-18 21:52:06 -0600

    Adding notes

--- practiceInCodedex.js
+++ practiceInCodedex.js
@@ -216,3 +216,22 @@ for (let i = 0; i < 5; i++) {
   }
   console.log(i);
 }
+
+//02/12/24
+//Breaking the loop
+let sum = 0;
+
+while (true) {
+    let value = +prompt("Enter a number", '');
+    if (!value) break; // (*)
+    sum += value;
+
+}
+alert('Sum: ' + sum);
+
+//continue 
+for (let i = 0; i < 10; i++) {
+    // if true, skip the remaining part of the body
+    if (i % 2 == 0) continue;
+    alert(i); // 1, then 3, 5, 7, 9
+}
