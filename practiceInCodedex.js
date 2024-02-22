1ebfba344bb4f523f921be884da32dfd0eaa5e68
Grym1314 <chucho_031199@hotmail.com>
2024-02-21 20:20:35 -0600

    My practice al lecture from React Documentation

--- practiceInCodedex.js
+++ practiceInCodedex.js
@@ -251,3 +251,42 @@ switch(x) {
     [break]
 }
 */
+
+
+//21/02/2024
+ //LIST
+ const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+//ClickCounter
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
