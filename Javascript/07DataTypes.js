3db781934367ed6e2e6421f7d2c1bf67db9592ba
Grym1314 <chucho_031199@hotmail.com>
2024-04-20 20:10:48 -0600

    chapter 7 studyng JavaScript in CODEDEX

---
+++ Javascript/07DataTypes.js
@@ -0,0 +1,21 @@
+//There are eight fundamental data types in JavaScrip
+//Numbers
+let year = 2023
+let age = 28
+let mealCost = 12.99
+const pi = 3.14
+const daysOfWeek = 7
+
+//Strings
+let message = "good nite"
+let user = '@sonnynomnom'
+const company = "Codédex"
+
+//Boolean
+let hungry = true
+let lateToMeeting = false
+const earthIsFlat = false
+
+//Null and undefined
+let newHire = null
+let startingDate = undefined
