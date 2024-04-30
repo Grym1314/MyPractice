37ce46ec22b166750aba5e38c140b76cd1ce20ec
Grym1314 <chucho_031199@hotmail.com>
2024-04-29 20:02:58 -0600

    Practice hook useEffect

---
+++ React/useEffect.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { createConnection } from './chat.js';
+
+function ChatRoom({ roomId }) {
+  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    return () => {
+      connection.disconnect();
+    };
+  }, [serverUrl, roomId]);
+  // ...
+};
+
+export default ChatRoom;
